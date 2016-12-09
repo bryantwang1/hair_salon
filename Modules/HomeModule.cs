8bff9dae46326e5840e5aee5f745489388a30ab4
Bryant Wang <bryant.m.wang@gmail.com>
2016-12-09 13:54:27 -0800

    Add client, stylist, stylist and client added, and clients cshtml pages.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -14,49 +14,58 @@ namespace HairSalon
                 return View["index.cshtml", allStylists];
             };
 
-            // Get["/clients"] = _ => {
-            //     List<Client> allClients = Client.GetAll();
-            //     return View["clients.cshtml", allClients];
-            // };
+            Get["/clients"] = _ => {
+                List<Client> allClients = Client.GetAll();
+                return View["clients.cshtml", allClients];
+            };
 
             Get["/stylists/new"] = _ => {
                 return View["stylist_form.cshtml"];
             };
 
-            // Post["/stylists/new/added"] = _ => {
-            //     string stylistName = Request.Form["stylist-name"];
-            //     string stylistPhone = Request.Form["stylist-phone"];
-            //     string stylistDescription = Request.Form["stylist-description"];
-            //
-            //     Stylist newStylist = new Stylist(stylistName, stylistPhone, stylistDescription);
-            //     newStylist.Save();
-            //     return View["stylist_added.cshtml", newStylist];
-            // };
+            Post["/stylists/new/added"] = _ => {
+                string stylistName = Request.Form["stylist-name"];
+                string stylistPhone = Request.Form["stylist-phone"];
+                string stylistDescription = Request.Form["stylist-description"];
+
+                Stylist newStylist = new Stylist(stylistName, stylistPhone, stylistDescription);
+                newStylist.Save();
+                return View["stylist_added.cshtml", newStylist];
+            };
 
             Get["/clients/new"] = _ => {
                 List<Stylist> allStylists = Stylist.GetAll();
                 return View["client_form.cshtml", allStylists];
             };
 
-            // Post["/clients/new/added"] = _ => {
-            //     string clientName = Request.Form["client-name"];
-            //     string clientDescription = Request.Form["client-description"];
-            //     string clientStylistId = Request.Form["client-stylist-id"];
-            //
-            //     Client newClient = newClient(clientName, clientDescription, clientStylistId);
-            //     newClient.Save();
-            //     return View["client_added.cshtml", newClient];
-            // }
-            //
-            // Get["/stylist/{id}"] = parameters => {
-            //     Stylist selectedStylist = Stylist.Find(parameters.id);
-            //     return View["stylist.cshtml", selectedStylist];
-            // };
-            //
-            // Get["/client/{id}"] = parameters => {
-            //     Client selectedClient = Client.Find(parameters.id);
-            //     return View["client.cshtml", selectedClient];
-            // };
+            Post["/clients/new/added"] = _ => {
+                Dictionary<string, object> model = new Dictionary<string, object> ();
+                string clientName = Request.Form["client-name"];
+                string clientDescription = Request.Form["client-description"];
+                int clientStylistId = int.Parse(Request.Form["client-stylist-id"]);
+
+                Client newClient = new Client(clientName, clientDescription, clientStylistId);
+                newClient.Save();
+
+                Stylist assignedStylist = Stylist.Find(clientStylistId);
+                model.Add("client", newClient);
+                model.Add("stylist", assignedStylist);
+                return View["client_added.cshtml", model];
+            };
+
+            Get["/stylist/{id}"] = parameters => {
+                Stylist selectedStylist = Stylist.Find(parameters.id);
+                return View["stylist.cshtml", selectedStylist];
+            };
+
+            Get["/client/{id}"] = parameters => {
+                Dictionary<string, object> model = new Dictionary<string, object> ();
+                Client selectedClient = Client.Find(parameters.id);
+                Stylist selectedStylist = Stylist.Find(selectedClient.GetStylistId());
+                model.Add("client", selectedClient);
+                model.Add("stylist", selectedStylist);
+                return View["client.cshtml", model];
+            };
             //
             // Get["/stylist/{id}/new_client"] = parameters => {
             //     Stylist selectedStylist = Stylist.Find(parameters.id);
